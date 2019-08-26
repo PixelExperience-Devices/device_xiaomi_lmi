6ea47d6d42d32f74fef1b9aba0dbdcadb260dee2
AbhiShek Aggarwal <warabhishek@gmail.com>
2019-08-26 17:40:14 -0400

    lmi: Add custom init to set proper variants
    Change-Id: I04f09a14cf41c89b3135c53e3089d5c3e703aae0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,10 @@ TARGET_OTA_ASSERT_DEVICE := lmi
 TARGET_BOOTLOADER_BOARD_NAME := kona
 TARGET_NO_BOOTLOADER := true
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_lmi
+TARGET_RECOVERY_DEVICE_MODULES := libinit_lmi
+
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm
 BOARD_KERNEL_IMAGE_NAME := Image
