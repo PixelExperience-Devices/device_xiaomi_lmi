90fee4032ae671cbc711132e5487dff3e5690bc3
XiNGRZ <chenxingyu92@gmail.com>
2019-12-28 01:47:21 +0800

    lmi: interfaces: Import goodix HAL definitions
    Change-Id: I989ed3920057728192536eeddde94e964692a4b6

--- interfaces/updates-makefiles.sh
+++ interfaces/updates-makefiles.sh
@@ -3,4 +3,7 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
+  "vendor.goodix:device/xiaomi/lmi/interfaces/goodix"
+
+do_makefiles_update \
   "vendor.xiaomi:device/xiaomi/lmi/interfaces/xiaomi"
