a389df33f20d1b2749f6db8c59e98f765d688383
Alexander Koskovich <zvnexus@outlook.com>
2021-03-20 19:08:05 -0700

    lmi: Import vendor WiFi & framework overlays.
     * From LA.UM.9.12.r1-12500-SMxx50.0
    
    Change-Id: I3d426d23fe8aa5d4c1c535b772fc426125fae715

--- device.mk
+++ device.mk
@@ -143,11 +143,13 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResCommon \
+    FrameworksResTarget \
     LmiFrameworks \
     LmiSystemUI \
     SystemUIResCommon \
     TelephonyResCommon \
-    WifiResCommon
+    WifiResCommon \
+    WifiResTarget
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
