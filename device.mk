b4038deb62167b841ffee9f9978d55ba14b3c75b
Alexander Koskovich <zvnexus@outlook.com>
2021-08-06 01:03:18 -0700

    lmi: Pick up QTI Bluetooth split makefiles.
     * This way we can skip out on defining a large part
       of packages/flags in tree.
    
    Change-Id: Ibb0854f553475a5b3647b9a34050711b77248e2e

--- device.mk
+++ device.mk
@@ -78,12 +78,20 @@ TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 # Bluetooth
+include vendor/qcom/opensource/commonsys-intf/bluetooth/bt-commonsys-intf-board.mk
+$(call inherit-product-if-exists, vendor/qcom/opensource/commonsys-intf/bluetooth/bt-system-opensource-product.mk)
+
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio@2.1-impl \
     com.dsi.ant@1.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
+
+ifneq (,$(wildcard vendor/qcom/opensource/commonsys/system/bt))
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.bluetooth.library_name=libbluetooth_qti.so
+endif
 
 # Camera
 PRODUCT_PACKAGES += \
