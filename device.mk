5c785c0a5a62751d7e2314660881bd110c8a1d73
Alexander Koskovich <zvnexus@outlook.com>
2021-09-17 12:38:40 +0800

    lmi: Disable the usage of ConfigStore.
    Change-Id: I5482989a8a96227a3e2a5317eacf613c641bfa8e

--- device.mk
+++ device.mk
@@ -105,6 +105,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
+# Configstore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
