1b592db1413ab6a78580219596f508636e42a98e
ZenkaBestia <z.mail.6600@gmail.com>
2021-10-03 13:18:01 +0000

    Revert "lmi: Build Snap camera app"
    This reverts commit 98f03858a330b01aa1891be0f5e3a607a19049f8.

--- device.mk
+++ device.mk
@@ -161,7 +161,6 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
     libgui_vendor \
-    Snap \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor \
     vendor.xiaomi.hardware.motor@1.0.vendor
