579855d7000833353037e17f75d868b1f02c5b34
ZenkaBestia <z.mail.6600@gmail.com>
2021-11-06 10:53:10 +0000

    lmi: update to Raven November 2021 fingerprint

--- nad_lmi.mk
+++ nad_lmi.mk
@@ -34,4 +34,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Redmi/lmi/lmi:11/RKQ1.200826.002/V12.5.4.0.RJKCNXM:user/release-keys
+BUILD_FINGERPRINT := "google/raven/raven:12/SD1A.210817.036/7805805:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="lmi-user 11 RKQ1.200826.002 V12.5.4.0.RJKCNXM release-keys"
