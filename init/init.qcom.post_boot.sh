df25cd4aa64c751f2a4587b73334c2f59ad6f504
dianlujitao <dianlujitao@lineageos.org>
2022-01-20 15:59:40 +0800

    lmi: init: Fix memory parameters for non-QCOM targets
    Change-Id: I896267e7bb3fd5e05ccfa2818d9df4b04205a7bc

--- init/init.qcom.post_boot.sh
+++ init/init.qcom.post_boot.sh
@@ -945,7 +945,7 @@ function configure_memory_parameters() {
     # Set allocstall_threshold to 0 for all targets.
     #
 
-ProductName=`getprop ro.product.name`
+ProductName=`getprop ro.board.platform`
 low_ram=`getprop ro.config.low_ram`
 
 if [ "$ProductName" == "msmnile" ] || [ "$ProductName" == "kona" ] || [ "$ProductName" == "sdmshrike_au" ]; then
