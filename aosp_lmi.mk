51fedb86ff848ab7eb8da29e44ba9b93d3fbc6d0
jackson177 <noor90saleh@gmail.com>
2022-03-15 13:20:47 +0000

    lmi: Update blobs from V12.5.3.0.RJKMIXM
    Signed-off-by: jackson177 <noor90saleh@gmail.com>

--- aosp_lmi.mk
+++ aosp_lmi.mk
@@ -28,7 +28,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys"
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220205.003 8069835 release-keys"
+    PRIVATE_BUILD_DESC="lmi-user 11 RKQ1.200826.002 V12.5.3.0.RJKMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/lmi/lmi:11/RKQ1.200826.002/V12.5.3.0.RJKMIXM:user/release-keys
