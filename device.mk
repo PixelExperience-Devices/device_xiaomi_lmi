cc4585ddcc296a52cd8d78aa8eb6ba677600dc48
Cosmin Tanislav <demonsingur@gmail.com>
2022-11-15 07:59:58 +0100

    lmi: Support UDFPS on AoD
    Change-Id: I7196dce967c9adbc62bb5156ade749c92c7780a1

--- device.mk
+++ device.mk
@@ -138,7 +138,11 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_lmi
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_lmi \
+    sensors.udfps.lmi
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # GPS
 PRODUCT_PACKAGES += \
