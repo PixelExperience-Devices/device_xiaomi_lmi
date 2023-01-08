e83994d4e5efbc6f4fe7e636573215b0acc6f76d
xenxynon <flynryder427@gmail.com>
2023-01-08 12:09:18 +0800

    lmi: Drop mock hidl power stats * E android.hardware.power.stats@1.0-service-mock Failed to getEnergyData

--- device.mk
+++ device.mk
@@ -391,8 +391,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi_lmi-libperfmgr \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power-service.xiaomi_lmi-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/aidl/power-libperfmgr/etc/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
