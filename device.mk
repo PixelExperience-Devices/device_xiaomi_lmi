d9bbfaab0971c1c4d749450d36bcbc52ecedbc65
Kyuofox <88403766@qq.com>
2023-02-18 17:55:10 +0800

    lmi: Import whitelist XML from QCOM
    * From LA.UM.9.12.r1-15100-SMxx50.0
    
    Signed-off-by: Kyuofox <88403766@qq.com>
    Change-Id: I0f3faa20fdb2c23c23087d394d5569ff24a59576

--- device.mk
+++ device.mk
@@ -129,6 +129,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2019-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# QTI
+PRODUCT_PACKAGES += \
+    qti_whitelist.xml
+
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
