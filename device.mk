d8db0a931aa098626d7725ad0508f9e365865fa2
Kyuofox <88403766@qq.com>
2023-02-18 18:39:11 +0800

    lmi: Import from native public libraries list from stock
    * From V14.0.1.0.SJKCNXM
    
    Signed-off-by: Kyuofox <88403766@qq.com>
    Change-Id: I320055cd44f911baad8740e8edba62c2ba2de54d

--- device.mk
+++ device.mk
@@ -103,6 +103,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Native Public Libraries
+PRODUCT_PACKAGES += \
+    public.libraries.txt
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
