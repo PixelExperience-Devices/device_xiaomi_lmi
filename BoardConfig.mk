20eabc0a01a9bc348edd56326400dc5c6a13652e
Kyuofox <88403766@qq.com>
2023-02-18 17:30:34 +0800

    lmi: Import xiaomi specific VINTF entries
    Change-Id: I7dbc365e686a51c36b710015e2de8be896ca05fb
    Signed-off-by: Kyuofox <88403766@qq.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,10 +33,12 @@ TARGET_NO_BOOTLOADER := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)configs/vintf//device_framework_matrix.xml
+    $(DEVICE_PATH)/configs/vintf/device_framework_matrix.xml \
+    $(DEVICE_PATH)/configs/vintf/framework_matrix_xiaomi.xml
 
 DEVICE_MANIFEST_FILE := \
-    $(DEVICE_PATH)/configs/vintf/manifest.xml
+    $(DEVICE_PATH)/configs/vintf/manifest.xml \
+    $(DEVICE_PATH)/configs/vintf/manifest_xiaomi.xml
 
 DEVICE_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
