09dbd40789e6057c12a33e788ee9198a8e0fbd97
dianlujitao <dianlujitao@lineageos.org>
2023-04-01 14:44:07 +0800

    lmi: Replace TMPDIR with EXTRACT_TMP_DIR
    Change-Id: I66146a0805a830ccf24db6e4cd57b229481ba6b0

--- extract-files.sh
+++ extract-files.sh
@@ -56,8 +56,8 @@ fi
 function blob_fixup() {
     case "${1}" in
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
-            hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/9A0A0094CE1640F9/1F2003D5CE1640F9/g" | xxd -r -p > "${TMPDIR}/${1##*/}"
-            mv "${TMPDIR}/${1##*/}" "${2}"
+            hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/9A0A0094CE1640F9/1F2003D5CE1640F9/g" | xxd -r -p > "${EXTRACT_TMP_DIR}/${1##*/}"
+            mv "${EXTRACT_TMP_DIR}/${1##*/}" "${2}"
             ;;
         vendor/lib64/camera/components/com.mi.node.watermark.so )
             $PATCHELF --add-needed "libpiex_shim.so" "${2}"
