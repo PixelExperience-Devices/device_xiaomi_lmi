Kyuofox-Studio/android_device_xiaomi_lmi

lmi: power-libperfmgr: Drop display low power feature
Signed-off-by: Kujou Yuko <i@779.moe>
Change-Id: Id4dc4045e449edd61cd2d3dd48ccdbbb0df38bec

power-libperfmgr/Power.cpp
@@ -30,7 +30,6 @@
 
 #include "PowerHintSession.h"
 #include "PowerSessionManager.h"
-#include "DisplayLowPower.h"
 
 namespace aidl {
 namespace google {
@@ -47,9 +46,8 @@ constexpr char kPowerHalRenderingProp[] = "vendor.powerhal.rendering";
 constexpr char kPowerHalAdpfRateProp[] = "vendor.powerhal.adpf.rate";
 constexpr int64_t kPowerHalAdpfRateDefault = -1;
 
-Power::Power(std::shared_ptr<HintManager> hm, std::shared_ptr<DisplayLowPower> dlpw)
+Power::Power(std::shared_ptr<HintManager> hm)
     : mHintManager(hm),
-      mDisplayLowPower(dlpw),
       mInteractionHandler(nullptr),
       mVRModeOn(false),
       mSustainedPerfModeOn(false),
@@ -96,14 +94,6 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
     LOG(DEBUG) << "Power setMode: " << toString(type) << " to: " << enabled;
     PowerSessionManager::getInstance()->updateHintMode(toString(type), enabled);
     switch (type) {
-        case Mode::LOW_POWER:
-            mDisplayLowPower->SetDisplayLowPower(enabled);
-            if (enabled) {
-                mHintManager->DoHint(toString(type));
-            } else {
-                mHintManager->EndHint(toString(type));
-            }
-            break;
         case Mode::SUSTAINED_PERFORMANCE:
             if (enabled && !mSustainedPerfModeOn) {
                 if (!mVRModeOn) {  // Sustained mode only.
@@ -181,10 +171,6 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
 
 ndk::ScopedAStatus Power::isModeSupported(Mode type, bool *_aidl_return) {
     bool supported = mHintManager->IsHintSupported(toString(type));
-    // LOW_POWER handled insides PowerHAL specifically
-    if (type == Mode::LOW_POWER) {
-        supported = true;
-    }
     LOG(INFO) << "Power mode " << toString(type) << " isModeSupported: " << supported;
     *_aidl_return = supported;
     return ndk::ScopedAStatus::ok();

power-libperfmgr/Power.h
@@ -23,7 +23,6 @@
 #include <aidl/android/hardware/power/BnPower.h>
 #include <perfmgr/HintManager.h>
 
-#include "DisplayLowPower.h"
 #include "InteractionHandler.h"
 
 namespace aidl {
@@ -40,7 +39,7 @@ using ::android::perfmgr::HintManager;
 
 class Power : public ::aidl::android::hardware::power::BnPower {
   public:
-    Power(std::shared_ptr<HintManager> hm, std::shared_ptr<DisplayLowPower> dlpw);
+    Power(std::shared_ptr<HintManager> hm);
     ndk::ScopedAStatus setMode(Mode type, bool enabled) override;
     ndk::ScopedAStatus isModeSupported(Mode type, bool *_aidl_return) override;
     ndk::ScopedAStatus setBoost(Boost type, int32_t durationMs) override;
@@ -54,7 +53,6 @@ class Power : public ::aidl::android::hardware::power::BnPower {
 
   private:
     std::shared_ptr<HintManager> mHintManager;
-    std::shared_ptr<DisplayLowPower> mDisplayLowPower;
     std::unique_ptr<InteractionHandler> mInteractionHandler;
     std::atomic<bool> mVRModeOn;
     std::atomic<bool> mSustainedPerfModeOn;

power-libperfmgr/service.cpp
@@ -26,9 +26,7 @@
 #include "Power.h"
 #include "PowerExt.h"
 #include "PowerSessionManager.h"
-#include "DisplayLowPower.h"
 
-using aidl::google::hardware::power::impl::pixel::DisplayLowPower;
 using aidl::google::hardware::power::impl::pixel::Power;
 using aidl::google::hardware::power::impl::pixel::PowerExt;
 using aidl::google::hardware::power::impl::pixel::PowerHintMonitor;
@@ -52,17 +50,15 @@ int main() {
         LOG(FATAL) << "Invalid config: " << config_path;
     }
 
-    std::shared_ptr<DisplayLowPower> dlpw = std::make_shared<DisplayLowPower>();
-
     // single thread
     ABinderProcess_setThreadPoolMaxThreadCount(0);
 
     // core service
-    std::shared_ptr<Power> pw = ndk::SharedRefBase::make<Power>(hm, dlpw);
+    std::shared_ptr<Power> pw = ndk::SharedRefBase::make<Power>(hm);
     ndk::SpAIBinder pwBinder = pw->asBinder();
 
     // extension service
-    std::shared_ptr<PowerExt> pwExt = ndk::SharedRefBase::make<PowerExt>(hm, dlpw);
+    std::shared_ptr<PowerExt> pwExt = ndk::SharedRefBase::make<PowerExt>(hm);
 
     // attach the extension to the same binder we will be registering
     CHECK(STATUS_OK == AIBinder_setExtension(pwBinder.get(), pwExt->asBinder().get()));
@@ -80,7 +76,6 @@ int main() {
     std::thread initThread([&]() {
         ::android::base::WaitForProperty(kPowerHalInitProp.data(), "1");
         hm->Start();
-        dlpw->Init();
     });
     initThread.detach();
 

PixelExperience-Devices/device_xiaomi_lmi

power-libperfmgr/aidl/Power.cpp
@@ -30,7 +30,6 @@
 
 #include "PowerHintSession.h"
 #include "PowerSessionManager.h"
-#include "disp-power/DisplayLowPower.h"
 
 namespace aidl {
 namespace google {
@@ -46,9 +45,8 @@ constexpr char kPowerHalStateProp[] = "vendor.powerhal.state";
 constexpr char kPowerHalAudioProp[] = "vendor.powerhal.audio";
 constexpr char kPowerHalRenderingProp[] = "vendor.powerhal.rendering";
 
-Power::Power(std::shared_ptr<DisplayLowPower> dlpw, std::shared_ptr<AdaptiveCpu> adaptiveCpu)
-    : mDisplayLowPower(dlpw),
-      mAdaptiveCpu(adaptiveCpu),
+Power::Power(std::shared_ptr<AdaptiveCpu> adaptiveCpu)
+    : mAdaptiveCpu(adaptiveCpu),
       mInteractionHandler(nullptr),
       mVRModeOn(false),
       mSustainedPerfModeOn(false) {
@@ -93,14 +91,6 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
         PowerSessionManager::getInstance()->updateHintMode(toString(type), enabled);
     }
     switch (type) {
-        case Mode::LOW_POWER:
-            mDisplayLowPower->SetDisplayLowPower(enabled);
-            if (enabled) {
-                HintManager::GetInstance()->DoHint(toString(type));
-            } else {
-                HintManager::GetInstance()->EndHint(toString(type));
-            }
-            break;
         case Mode::SUSTAINED_PERFORMANCE:
             if (enabled && !mSustainedPerfModeOn) {
                 if (!mVRModeOn) {  // Sustained mode only.
@@ -180,10 +170,6 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
 
 ndk::ScopedAStatus Power::isModeSupported(Mode type, bool *_aidl_return) {
     bool supported = HintManager::GetInstance()->IsHintSupported(toString(type));
-    // LOW_POWER handled insides PowerHAL specifically
-    if (type == Mode::LOW_POWER) {
-        supported = true;
-    }
     LOG(INFO) << "Power mode " << toString(type) << " isModeSupported: " << supported;
     *_aidl_return = supported;
     return ndk::ScopedAStatus::ok();

power-libperfmgr/aidl/Power.h
@@ -23,7 +23,6 @@
 #include <thread>
 
 #include "adaptivecpu/AdaptiveCpu.h"
-#include "disp-power/DisplayLowPower.h"
 #include "disp-power/InteractionHandler.h"
 
 namespace aidl {
@@ -39,7 +38,7 @@ using ::aidl::android::hardware::power::Mode;
 
 class Power : public ::aidl::android::hardware::power::BnPower {
   public:
-    Power(std::shared_ptr<DisplayLowPower> dlpw, std::shared_ptr<AdaptiveCpu> adaptiveCpu);
+    Power(std::shared_ptr<AdaptiveCpu> adaptiveCpu);
     ndk::ScopedAStatus setMode(Mode type, bool enabled) override;
     ndk::ScopedAStatus isModeSupported(Mode type, bool *_aidl_return) override;
     ndk::ScopedAStatus setBoost(Boost type, int32_t durationMs) override;
@@ -52,7 +51,6 @@ class Power : public ::aidl::android::hardware::power::BnPower {
     binder_status_t dump(int fd, const char **args, uint32_t numArgs) override;
 
   private:
-    std::shared_ptr<DisplayLowPower> mDisplayLowPower;
     std::shared_ptr<AdaptiveCpu> mAdaptiveCpu;
     std::unique_ptr<InteractionHandler> mInteractionHandler;
     std::atomic<bool> mVRModeOn;

power-libperfmgr/aidl/service.cpp
@@ -29,10 +29,8 @@
 #include "PowerExt.h"
 #include "PowerSessionManager.h"
 #include "adaptivecpu/AdaptiveCpu.h"
-#include "disp-power/DisplayLowPower.h"
 
 using aidl::google::hardware::power::impl::pixel::AdaptiveCpu;
-using aidl::google::hardware::power::impl::pixel::DisplayLowPower;
 using aidl::google::hardware::power::impl::pixel::Power;
 using aidl::google::hardware::power::impl::pixel::PowerExt;
 using aidl::google::hardware::power::impl::pixel::PowerHintMonitor;
@@ -48,20 +46,18 @@ int main() {
         LOG(FATAL) << "HintManager Init failed";
     }
 
-    std::shared_ptr<DisplayLowPower> dlpw = std::make_shared<DisplayLowPower>();
-
     // single thread
     ABinderProcess_setThreadPoolMaxThreadCount(0);
 
     std::shared_ptr<AdaptiveCpu> adaptiveCpu = std::make_shared<AdaptiveCpu>();
 
     // core service
-    std::shared_ptr<Power> pw = ndk::SharedRefBase::make<Power>(dlpw, adaptiveCpu);
+    std::shared_ptr<Power> pw = ndk::SharedRefBase::make<Power>(adaptiveCpu);
     ndk::SpAIBinder pwBinder = pw->asBinder();
     AIBinder_setMinSchedulerPolicy(pwBinder.get(), SCHED_NORMAL, -20);
 
     // extension service
-    std::shared_ptr<PowerExt> pwExt = ndk::SharedRefBase::make<PowerExt>(dlpw, adaptiveCpu);
+    std::shared_ptr<PowerExt> pwExt = ndk::SharedRefBase::make<PowerExt>(adaptiveCpu);
     auto pwExtBinder = pwExt->asBinder();
     AIBinder_setMinSchedulerPolicy(pwExtBinder.get(), SCHED_NORMAL, -20);
 
@@ -80,7 +76,6 @@ int main() {
     std::thread initThread([&]() {
         ::android::base::WaitForProperty(kPowerHalInitProp.data(), "1");
         HintManager::GetInstance()->Start();
-        dlpw->Init();
     });
     initThread.detach();
 