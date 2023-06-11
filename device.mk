Kyuofox-Studio/android_device_xiaomi_lmi

lmi: Build mock power stats HAL and label it
Fixes the following logspam:
* Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.
* BatteryStatsService: Unable to load Power Hal or power.stats HAL

Change-Id: I721b832f4abf1f31bc5e2a9199cb31f757d9b51b

device.mk
@@ -364,7 +364,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/etc/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json

PixelExperience-Devices/device_xiaomi_lmi

device.mk
@@ -342,7 +342,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi_lmi-libperfmgr
+    android.hardware.power-service.xiaomi_lmi-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/etc/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json