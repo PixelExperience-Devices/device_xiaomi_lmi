ec3a531ee3c5612a6be801272b564d4d6c6edeef
ZenkaBestia <z.mail.6600@gmail.com>
2023-06-23 12:46:44 +0800

    lmi: add vendorsetup and remove.txt - Track kernel repository - Track vendor repository - Track custom apps repository

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+echo 'Cloning Kernel Tree'
+# Kernel Tree
+git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_xiaomi_lmi.git -b zen_plus-13 kernel/xiaomi/lmi
+
+echo 'Cloning Vendor Tree'
+# Vendor Tree
+git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_lmi.git -b Tiramisu vendor/xiaomi/lmi
+
+echo 'Clone Prebuilt Apps Repo'
+# Prebuilt Apps
+git clone --depth=1 https://gitlab.com/ZenkaBestia/device_xiaomi_lmi_prebuilt-apps -b main device/xiaomi/lmi-prebuilt-apps
