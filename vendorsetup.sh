5231ca29878b1d0ae78f7ff52c0949db8d1c8682
ZenkaBestia <z.mail.6600@gmail.com>
2023-06-23 21:54:07 +0800

    lmi: use Kitsune as default kernel * source repo at https://github.com/Kyuofox-Studio/android_kernel_xiaomi_lmi/tree/lineage-20

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 echo 'Cloning Kernel Tree'
 # Kernel Tree
-git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_xiaomi_lmi.git -b zen_plus-13 kernel/xiaomi/lmi
+git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_xiaomi_lmi.git -b Kitsune kernel/xiaomi/lmi
 
 echo 'Cloning Vendor Tree'
 # Vendor Tree
