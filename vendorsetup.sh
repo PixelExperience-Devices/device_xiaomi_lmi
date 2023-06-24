6bea1bc803e76fb93592920d104d6ab608497164
ZenkaBestia <z.mail.6600@gmail.com>
2023-06-24 16:18:51 +0800

    lmi: use audio hal from Void UI

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,3 +9,7 @@ git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_lmi.g
 echo 'Clone Prebuilt Apps Repo'
 # Prebuilt Apps
 git clone --depth=1 https://gitlab.com/ZenkaBestia/device_xiaomi_lmi_prebuilt-apps -b main device/xiaomi/lmi-prebuilt-apps
+
+echo 'Clone Audio Hal Repo'
+# Audio
+git clone --depth=1 https://github.com/VoidUI-Tiramisu/hardware_qcom-caf_sm8250_audio -b aosp-13 hardware/qcom-caf/sm8250/audio
