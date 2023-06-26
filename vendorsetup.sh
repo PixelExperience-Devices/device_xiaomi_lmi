b9549d725ff1e404e8dc1560707ac8f5cebe5e38
AbrarNoob <hasinabrarhaque@gmail.com>
2023-06-26 18:43:27 +0800

    lmi: use Lineage compat repo * fix camera issue in 3rd party apps

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,3 +13,6 @@ git clone --depth=1 https://gitlab.com/ZenkaBestia/device_xiaomi_lmi_prebuilt-ap
 echo 'Clone Audio Hal Repo'
 # Audio
 git clone --depth=1 https://github.com/VoidUI-Tiramisu/hardware_qcom-caf_sm8250_audio -b aosp-13 hardware/qcom-caf/sm8250/audio
+
+echo 'Clone Lineage Compat Repo'
+git clone --depth=1 https://github.com/LineageOS/android_hardware_lineage_compat -b lineage-20.0 hardware/lineage/compat
