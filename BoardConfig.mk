446084214c6e50af6b19ef2b6d183f9eb0673ea5
Kyuofox <kyuofox@gmail.com>
2023-09-01 10:22:00 +0800

    lmi: Switch to kernel based DT2W
    Change-Id: Iff540b70ab0e4eb66c3df3d1afc8129402573dcf
    Signed-off-by: Kyuofox <kyuofox@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,7 +154,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 TARGET_BOARD_PLATFORM := kona
 
 # Power
-TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/configs/properties/odm.prop
