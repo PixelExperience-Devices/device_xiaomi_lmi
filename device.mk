8ae34c1afc93a4af406adeefc009eb252f8f7775
Kyuofox <kyuofox@gmail.com>
2023-10-29 16:37:23 +0800

    lmi: Build android.hardware.nfc@1.2 service
    Change-Id: Ic9bf89f6d91713beb8214a853515d79b47a658de

--- device.mk
+++ device.mk
@@ -276,6 +276,8 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.2.vendor \
+    android.hardware.nfc@1.2-service \
     android.hardware.nfc-service.nxp \
     android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
