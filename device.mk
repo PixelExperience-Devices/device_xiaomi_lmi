a28e116ed46be18cc74f8d22d9eb0aa442e93a2f
johnmart19 <johnivan19999@gmail.com>
2023-11-05 08:36:25 +0000

    lmi: Build legacy android.frameworks.sensorservice@1.0
    init: Control message: Could not find 'android.frameworks.sensorservice@1.0::ISensorManager/default' for ctl.interface_start from pid: 638 (/system/bin/hwservicemanager)

--- device.mk
+++ device.mk
@@ -430,6 +430,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@1.0-impl.xiaomi_lmi \
     android.hardware.sensors@1.0-service \
